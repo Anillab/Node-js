c7310240b192c9a4cd9ff16be2e36df447186444
Anillab <anillabusolo@gmail.com>
2019-10-21 19:43:06 +0000

    parsing the urlpath and receiving the response

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ const url=require('url');
 // create a server and configure where it listens
 
 const server=http.createServer((req,res) =>{
+  // get the url and parse it
+  const parsedUrl=url.parse(req.url,true,true);
+  // get the path and trim it to cater for  any slashes added
+  const urlPath=parsedUrl.pathname;
+  const trimUrl=urlPath.replace(/^\/+|\/+$/g,'');
+  // send the response
   res.end('Hello World!\n')
+  // log the path
+  console.log(`path is ${trimUrl}`);
 }).listen(4000)
