220787eccb0eb21b002ef565318ccdd5e1766122
Anillab <anillabusolo@gmail.com>
2019-10-28 07:17:49 +0000

    payloads

--- Restful_Api/index.js
+++ Restful_Api/index.js
@@ -2,7 +2,7 @@
 
 const http=require('http');
 const url=require('url');
-const {StringDecoder}=require('string_decoder');
+const StringDecoder=require('string_decoder').StringDecoder;
 
 // create a server and configure where it listens
 
@@ -20,22 +20,21 @@ const server=http.createServer((req,res) =>{
   const headers=req.headers;
   // payloads
   const decoder=new StringDecoder('utf8');
-  const buffer= '';
-  decoder.write(Buffer.from(req.on))
-  // req.on('data',function(data){
-  //   buffer += decoder.write(data);
-  // });
+  const buffer ='';
 
-  console.log(decoder.end(Buffer.from(req.on)));
-  // req.on('end',function(){
-  //   buffer +=decoder.end();
-  //   // send the response
-  //   res.end('Hello World!\n')
-  //   console.log(`path is ${trimUrl} with method ${method} and payloads ${buffer} `);
-  //
-  // });
+  req.on('data',function(data){
+    buffer +=decoder.write(data)
+  });
+
+  req.on('end',function(){
+     buffer +=decoder.end();
+    // send the response
+    res.end('Hello World!\n')
+    console.log(buffer);
+
+  });
 
   // log the path
-  console.dir(queryStringObject);
-  console.dir(headers);
+  // console.dir(queryStringObject);
+  // console.dir(headers);
 }).listen(4000)
